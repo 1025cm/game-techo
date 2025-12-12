9fa1592a512662ad83b610194baca23b9a239363
1025cm <panda_cm@icloud.com>
2025-12-12 21:15:23 +0900

    fix build script and settings

--- game_techo/settings.py
+++ game_techo/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'game_techo.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = 
-    default_db_config = {
+default_db_config = {
     'ENGINE': 'django.db.backends.sqlite3',
     'NAME': BASE_DIR / 'db.sqlite3',
 }
