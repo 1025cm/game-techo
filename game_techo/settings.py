c072cf3ca1f29ff548413f80839776418c11191e
1025cm <panda_cm@icloud.com>
2025-12-12 21:08:51 +0900

    fix settings syntax error

--- game_techo/settings.py
+++ game_techo/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'game_techo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
+DATABASES = 
     default_db_config = {
     'ENGINE': 'django.db.backends.sqlite3',
     'NAME': BASE_DIR / 'db.sqlite3',
@@ -84,7 +84,6 @@ DATABASES = {
 
 DATABASES = {
     'default': dj_database_url.config(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}", conn_max_age=600)
-}
 }
 
 
